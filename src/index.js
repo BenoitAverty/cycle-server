f18220939eba24656444d3b646db350e8ef06485
Benoit Averty <benoit.averty@gmail.com>
2016-09-15 16:22:03 +0200

    Temporary usage ideas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import winston from 'winston';
 
+// This would be a npm package
+import makeCycleMiddleWare from './cycle-server';
+
+// This is the app.
+import cycleApp from './cycle-app';
+
+// Create the app
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// create the middleWare. This will also run the app ?
+const otherDrivers = {
+  // drivers (HTTP, mongo...)
+};
+const cycleMiddleWare = makeCycleMiddleWare(cycleApp, otherDrivers);
+
+app.use('/api', cycleMiddleWare);
 
 app.listen(3000, () => {
   winston.info('Example app listening on port 3000!');
